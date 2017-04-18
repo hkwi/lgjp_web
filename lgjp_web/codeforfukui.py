3238b04aa04067e8a19202201d3c858d21e22b74
Kawai, Hiroaki <hiroaki.kawai@gmail.com>
2017-04-18 22:39:50 +0900

    strip "SHI" level names in "KU"

--- lgjp_web/codeforfukui.py
+++ lgjp_web/codeforfukui.py
@@ -8,8 +8,14 @@ except:
 	from urllib import urlopen
 
 remote = "https://raw.githubusercontent.com/codeforfukui/localgovjp/gh-pages/localgovjp-utf8.csv"
-base = ["%s,%s" % (r["city"],r["url"])
-	for r in csv.DictReader(io.TextIOWrapper(urlopen(remote)))]
+base = []
+for r in csv.DictReader(io.TextIOWrapper(urlopen(remote))):
+	city_pair = r["city"].split()
+	if len(city_pair) > 1:
+		base.append("%s,%s" % (city_pair[1], r["url"]))
+	else:
+		base.append("%s,%s" % (r["city"], r["url"]))
+	
 ex = ["%s,%s" % (name.value, str(site))
 	for code, name, site in lgjp_web.wd.info if code[2:5] != "000"]
 with open("docs/codeforfukui.diff","w") as w:
