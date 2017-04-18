1686711c7435d4bfe4867396458afa8981254668
Kawai, Hiroaki <hiroaki.kawai@gmail.com>
2017-04-18 23:11:42 +0900

    add output file

--- auto.sh
+++ auto.sh
@@ -17,5 +17,6 @@ git config user.email "hiroaki.kawai@gmail.com"
 git checkout master
 git add docs/wd.csv
 git add docs/codeforfukui.diff
+git add docs/codeforfukui_dns.diff
 git commit -m "auto"
 git push "https://hkwi:${GH_TOKEN}@github.com/hkwi/lgjp_web.git" master:master > /dev/null 2>&1
