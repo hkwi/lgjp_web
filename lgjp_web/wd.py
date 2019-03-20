68d82d9b19901a190bf12d2c5b45fd7cec868dbe
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2019-03-20 12:35:34 +0900

    add SPARQL filter

--- lgjp_web/wd.py
+++ lgjp_web/wd.py
@@ -9,11 +9,13 @@ SELECT ?code ?name ?site WHERE {
  ?s wdt:P429 ?code ; # 全国地方公共団体コード
     rdfs:label ?name .
   OPTIONAL {
-    ?s wdt:P856 ?site . # 公式サイト
+    ?s p:P856 ?stmt .
+    ?stmt ps:P856 ?site . # 公式サイト
   }
  FILTER ( lang(?name)="ja" )
  FILTER NOT EXISTS { ?s wdt:P31 wd:Q18663566 } # 分類 日本の廃止市町村
  FILTER NOT EXISTS { ?s wdt:P31 wd:Q850450 } # 分類 支庁
+ FILTER NOT EXISTS { ?stmt wdt:P642 ?x } # ～についての
 } ORDER BY ?code ?site
 ''')
 open("docs/wd.csv","w").write(info.serialize(format="csv").decode("UTF-8"))
