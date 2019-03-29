4597f422bfa609ee28a7d69512bc46b43c93d8b1
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2019-03-29 21:26:12 +0900

    add http GET test (conditional)

--- test/test_hb.py
+++ test/test_hb.py
@@ -5,6 +5,8 @@ import logging
 import socket
 import unittest
 import concurrent.futures
+import requests
+import yaml
 from urllib.parse import urlparse, urlencode
 from urllib.request import urlopen
 
@@ -50,3 +52,54 @@ class TestAccess(unittest.TestCase):
 		errors = [x for x in r if x]
 		if errors:
 			raise Exception(repr(errors))
+
+	@unittest.skipUnless(os.environ.get("FULLSCAN") , "heavy")
+	def test_get(self):
+		import lgjp_web.wd
+		def func(arg):
+			code, name, site = arg
+			info = {}
+			
+			entr = str(site)
+			try:
+				r = requests.get(entr)
+				assert r.ok, r.reason
+				fin = r.url
+				
+				a = urlparse(entr)
+				b = urlparse(fin)
+				
+				if a.netloc != b.netloc:
+					if "www."+a.netloc == b.netloc:
+						pass
+					elif a.netloc == "www."+b.netloc:
+						pass
+					else:
+						info["MOVE"] = {"enter": entr, "final": fin}
+					return info
+				
+				if a.scheme=="http" and b.scheme=="https":
+					info["HTTPS"] = entr
+				elif a.scheme=="https" and b.scheme=="http":
+					info["HTTP"] = entr
+				elif a.scheme != b.scheme:
+					info["SCHEME"] = entr
+				
+			except Exception as e:
+				info["ERROR"] = {"enter": entr, "reason": str(e)}
+			
+			return info
+		
+		p = concurrent.futures.ThreadPoolExecutor(10)
+		r = p.map(func, lgjp_web.wd.info)
+		errors = [x for x in r if x]
+		
+		bulk = {}
+		for k in ("ERROR", "MOVE", "SCHEME", "HTTP", "HTTPS"):
+			bulk[k] = [e[k] for e in errors if k in e]
+		
+		if errors:
+			raise Exception(yaml.dump(bulk))
+
+if __name__=="__main__":
+	unittest.main()
