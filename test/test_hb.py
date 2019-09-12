b6e0d7b177c739eb8d8dfff85327ff172ce1c296
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2019-09-12 12:39:42 +0900

    use lgjp_web.wd

--- test/test_hb.py
+++ test/test_hb.py
@@ -56,24 +56,8 @@ class TestAccess(unittest.TestCase):
 
 	@unittest.skipUnless(os.environ.get("FULLSCAN") , "heavy")
 	def test_fullscan(self):
-		import rdflib
-		g = rdflib.ConjunctiveGraph(store="SPARQLStore")
-		g.store.endpoint = "https://query.wikidata.org/bigdata/namespace/wdq/sparql"
-		info = g.query('''
-		SELECT ?s ?name ?site WHERE {
-		 ?s wdt:P429 ?code ; # 全国地方公共団体コード
-		    rdfs:label ?name .
-		  OPTIONAL {
-		    ?s p:P856 ?stmt .
-		    ?stmt ps:P856 ?site . # 公式サイト
-		    FILTER NOT EXISTS { ?stmt pq:P642 ?x } # ～についての
-		    FILTER NOT EXISTS { ?stmt pq:P582 ?y } # 終了日
-		  }
-		 FILTER ( lang(?name)="ja" )
-		 FILTER NOT EXISTS { ?s wdt:P31 wd:Q18663566 } # 分類 日本の廃止市町村
-		 FILTER NOT EXISTS { ?s wdt:P31 wd:Q850450 } # 分類 支庁
-		} ORDER BY ?code ?site
-		''')
+		import lgjp_web.wd
+		info = lgjp_web.wd.info
 		
 		def func(arg):
 			qname, name, site = arg
